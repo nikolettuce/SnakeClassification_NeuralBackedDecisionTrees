3134e7c90c446844163253a158b7462cdf4a96cf
cedricchao <49173387+cedricchao@users.noreply.github.com>
2021-02-25 14:44:53 -0800

    Update loss.py

--- src/model/loss.py
+++ src/model/loss.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
-from nbdt.model import SoftTreeSupLoss
+# from nbdt.model import SoftTreeSupLoss
+from nbdt.loss import SoftTreeSupLoss
+from nbdt.loss import HardTreeSupLoss
 
 def SoftTreeLoss_wrapper(data_cfg):
     '''
@@ -10,6 +12,24 @@ def SoftTreeLoss_wrapper(data_cfg):
     '''
     criterion = nn.CrossEntropyLoss()
     criterion = SoftTreeSupLoss(
+        dataset = data_cfg['dataset'],
+        hierarchy='induced-densenet121',
+        path_graph = os.path.join(data_cfg['hierarchyPath'], data_cfg['hierarchyJSON']),
+        path_wnids = data_cfg[wnidPath],
+        criterion = criterion
+    )
+    
+    return criterion
+
+def HardTreeLoss_wrapper(data_cfg):
+    '''
+    Creates HardTreeSupLoss wrapper for our dataset from the nbdt package
+    
+    returns:
+    criterion - HardTreeSupLoss
+    '''
+    criterion = nn.CrossEntropyLoss()
+    criterion = HardTreeSupLoss(
         dataset = data_cfg['dataset'],
         hierarchy='induced-densenet121',
         path_graph = os.path.join(data_cfg['hierarchyPath'], data_cfg['hierarchyJSON']),
